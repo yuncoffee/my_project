4d6137324ceb35a9e5e9eae9a12d6cf9737c1745
coffeegom <dong072815@gmail.com>
2021-08-15 14:26:20 +0900

    [update] 아아 하기시러~

---
+++ js/carousel.js
@@ -0,0 +1,42 @@
+// const carousel = document.querySelector('.carousel');
+// const carouselList = document.querySelector('.carousel-list');
+// const sliderVisibleWidth = carousel.offsetWidth;
+// const slider = carousel.querySelector('.carousel-listWrap');
+// const items = carousel.querySelectorAll('.carouselItem');
+// console.log(items);
+
+// function getTotalItemsWidth(items) {
+//     const { left } = items[0].getBoundingClientRect();
+//     const { right } = items[items.length - 1].getBoundingClientRect();
+//     return right - left;
+//   }
+
+//   getTotalItemsWidth(items);
+
+// function carousel2(container) {
+//     const slider = carousel.querySelector('.carousel-listWrap');
+//     const items = carousel.querySelectorAll('.carouselItem');
+
+
+//       console.log(items);
+// }
+
+const prevBtn = document.querySelector(".btn-prev");
+const nextBtn = document.querySelector(".btn-next"); 
+const container = document.querySelector(".carousel-listWrap");
+
+function 콜백함수(){
+    console.log("a");
+    container.style.transform = `translateX(-1000px)`;
+}
+
+function addEvent(){
+  prevBtn.addEventListener('click', 콜백함수());
+  nextBtn.addEventListener('click', 콜백함수());
+};
+
+
+
+// container.style.transform = translateX(4000);
+
+// container.style.transform = `translateX(${direction * (100 / 요소의 갯수)}%)`;
