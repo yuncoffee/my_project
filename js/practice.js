077106590527f1a1067f50701c6a58aa12c5c9a8
coffeegom <dong072815@gmail.com>
2021-09-05 23:33:33 +0900

    커밋부탁해용 ㅎ

---
+++ js/practice.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector('.btn');
+console.log(btn);
+
+let body1 = document.querySelector('.modal-body');
+console.log(body1);
+
+let body2 = document.querySelector('.modal-body2');
+console.log(body2);
+
+let body3 = document.querySelector('.modal-body3');
+console.log(body2);
+
+btn.addEventListener('click', function(){
+    body1.classList.add('focus');
+    setTimeout(function(){
+        body2.classList.add('focus');
+    }, 1000);
+    // setTimeout(function(){
+    //     body2.classList.remove('focus');
+    // }, 3000);
+    // setTimeout(function(){
+    //     body3.classList.add('focus');
+    // }, 4000);
+    
+});
